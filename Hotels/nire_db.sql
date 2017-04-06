cef3a2369508294d6fe7b8f01f3fd11a487e7741
petxa01 <petxa01@gmail.com>
2017-04-06 13:19:41 +0200

    move some things

--- Hotels/nire_db.sql
+++ Hotels/nire_db.sql
@@ -1,47 +1,55 @@
-Drop table Employees;
-create table Employees(
-  Nan VARCHAR (9) PRIMARY KEY,
-  Name TEXT(50),
-  Surname1 Text(50),
+drop table reservation;
+create table reservation(
+  resCod int(5) PRIMARY KEY,
+  customer_nan VARCHAR(9),
+  roomNumber int(4),
+  enterDate date,
+  leaveDate date,
+  FOREIGN KEY (customer_nan) REFERENCES customers(nan),
+  FOREIGN KEY (roomNumber) REFERENCES Rooms(roomNumber)
+);
+Drop table employees;
+create table employees(
+  nan VARCHAR (9) PRIMARY KEY,
+  name TEXT(50),
+  surname1 Text(50),
   surname2 text(50),
   phone int(9),
   email VARCHAR(50) UNIQUE,
   gender text(50),
-  Job_Type VARCHAR(50),
-  User_name VARCHAR(50) UNIQUE,
+  job_Type VARCHAR(50),
+  user_name VARCHAR(50) UNIQUE,
   user_pw VARCHAR(50),
   isAdmin boolean
 
 
 );
-drop table Customers;
-create table Customers(
-  Nan VARCHAR (9) PRIMARY KEY,
-  Name TEXT(50),
-  Surname1 Text(50),
+drop table customers;
+create table customers(
+  nan VARCHAR (9) PRIMARY KEY,
+  name TEXT(50),
+  surname1 Text(50),
   surname2 text(50),
   phone int(9),
   email VARCHAR(50) UNIQUE,
   gender text(50),
-  Payment_method VARCHAR(50),
-  User_name VARCHAR(50) UNIQUE,
+  payment_method VARCHAR(50),
+  user_name VARCHAR(50) UNIQUE,
   user_pw VARCHAR(50)
 
 );
-drop table Rooms;
-create table Rooms(
+drop table rooms;
+create table rooms(
   roomNumber int(4) PRIMARY KEY,
-  floor int(2),
+  floorNumber int(2) PRIMARY KEY,
   type_of_room VARCHAR (30)
 
 );
-drop table Reservations;
-create table Reservation(
-  resCod int(5) PRIMARY KEY,
-  customer_nan VARCHAR(9) FOREIGN KEY ,
-  roomNumber int(4) FOREIGN KEY,
-  enterDate date(),
-  leaveDate date(),
-  FOREIGN KEY (customer_nan) REFERENCES Customers(Nan),
-  FOREIGN KEY (roomNumber) REFERENCES Rooms(roomNumber)
-);
+insert into rooms values (1,1,"single");
+insert into rooms values (2,1,"single");
+insert into rooms values (3,1,"single");
+insert into rooms values (4,1,"single");
+insert into rooms values (5,1,"single");
+insert into rooms values (6,1,"single");
+insert into rooms values (1,2,"suit");
+insert into rooms values (2,2,"suit");
