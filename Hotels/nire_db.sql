eb93993c8b3fa4678d7a1774a8af37c0d412ce8c
petxa01 <petxa01@gmail.com>
2017-04-06 13:43:00 +0200

    fixed the drop table

--- Hotels/nire_db.sql
+++ Hotels/nire_db.sql
@@ -1,16 +1,22 @@
-drop table reservation;
+drop table IF EXISTS reservation;
 create table reservation(
-  resCod int(5) PRIMARY KEY,
-  customer_nan VARCHAR(9),
-  roomNumber int(4),
+  id int(5) NOT NULL AUTO_INCREMENT,
+  customer_id VARCHAR(9) UNIQUE,
+  room_id int(4),
   enterDate date,
   leaveDate date,
-  FOREIGN KEY (customer_nan) REFERENCES customers(nan),
-  FOREIGN KEY (roomNumber) REFERENCES Rooms(roomNumber)
+  PRIMARY KEY (id),
+  FOREIGN KEY (customer_id)
+    REFERENCES customers(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (room_id)
+   REFERENCES Rooms(id)
+   ON UPDATE CASCADE ON DELETE CASCADE
 );
-Drop table employees;
+Drop table IF EXISTS employees;
 create table employees(
-  nan VARCHAR (9) PRIMARY KEY,
+  id int(5) NOT NULL AUTO_INCREMENT,
+  nan VARCHAR (9) UNIQUE,
   name TEXT(50),
   surname1 Text(50),
   surname2 text(50),
@@ -20,13 +26,15 @@ create table employees(
   job_Type VARCHAR(50),
   user_name VARCHAR(50) UNIQUE,
   user_pw VARCHAR(50),
-  isAdmin boolean
+  isAdmin boolean,
+  PRIMARY KEY (id)
 
 
 );
-drop table customers;
+drop table IF EXISTS customers;
 create table customers(
-  nan VARCHAR (9) PRIMARY KEY,
+  id int(5) NOT NULL AUTO_INCREMENT,
+  nan VARCHAR (9) UNIQUE,
   name TEXT(50),
   surname1 Text(50),
   surname2 text(50),
@@ -35,21 +43,24 @@ create table customers(
   gender text(50),
   payment_method VARCHAR(50),
   user_name VARCHAR(50) UNIQUE,
-  user_pw VARCHAR(50)
+  user_pw VARCHAR(50),
+  PRIMARY KEY (id)
 
 );
-drop table rooms;
+drop table IF EXISTS rooms;
 create table rooms(
-  roomNumber int(4) PRIMARY KEY,
-  floorNumber int(2) PRIMARY KEY,
-  type_of_room VARCHAR (30)
+  id int(5) NOT NULL AUTO_INCREMENT,
+  roomNumber int(4) UNIQUE,
+  floorNumber int(2),
+  type_of_room VARCHAR (30),
+  PRIMARY KEY (id)
 
 );
-insert into rooms values (1,1,"single");
-insert into rooms values (2,1,"single");
-insert into rooms values (3,1,"single");
-insert into rooms values (4,1,"single");
-insert into rooms values (5,1,"single");
-insert into rooms values (6,1,"single");
-insert into rooms values (1,2,"suit");
-insert into rooms values (2,2,"suit");
+insert into rooms values (101,1,"single");
+insert into rooms values (102,1,"single");
+insert into rooms values (103,1,"single");
+insert into rooms values (104,1,"single");
+insert into rooms values (105,1,"single");
+insert into rooms values (106,1,"single");
+insert into rooms values (201,2,"suit");
+insert into rooms values (202,2,"suit");
