86b8d9be8a070c9fa59e318311b568fa75152767
petxa01 <petxa01@gmail.com>
2017-04-07 10:14:54 +0200

    template added

---
+++ Hotels/sessions_users_query.php
@@ -0,0 +1,16 @@
+<?php
+include ("test_conect_db.php");
+$link=conectDataBase();
+$result=mysqli_query($link, "select user_pw from user_enterprise where user_id=".$_POST["userid"]);
+$pw=mysqli_fetch_array($result);
+#sha1($pw); para cifrar contraseñas
+
+if ($pw["user_pw"]==$_POST["password"]) {
+  # code...
+  header("Location: welcome.php");
+}else {
+  # code...
+  header("Location: notloged.html");
+}
+
+ ?>
